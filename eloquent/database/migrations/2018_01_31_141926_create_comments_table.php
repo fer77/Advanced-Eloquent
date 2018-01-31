0877f5a5bbd07159945f8504b7ae295363077917
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-01-31 08:31:43 -0700

    created a migration for comments

--- eloquent/database/migrations/2018_01_31_141926_create_comments_table.php
+++ eloquent/database/migrations/2018_01_31_141926_create_comments_table.php
@@ -14,7 +14,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('post_id');
+            $table->integer('post_id')->unsigned();
             $table->foreign('post_id')->references('id')->on('posts');
             $table->text('body');
             $table->timestamps();
