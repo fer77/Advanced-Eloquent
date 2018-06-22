c8ea81f1d120e917ce6b70909644e0efc40b34d5
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-06-22 11:50:13 -0600

    added tracking to users that made changes to a document

--- eloquent-techniques/documents-lesson/app/Document.php
+++ eloquent-techniques/documents-lesson/app/Document.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    public function adjustments()
+    {
+        return $this->belongsToMany(User::class, 'adjustments')
+            ->withTimestamps()
+            ->latest('pivot_updated_at');
+    }
 }
